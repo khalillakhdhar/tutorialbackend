e435bc705d22da355a6b82faaf5ce314a5986704
khalil <khalillakhdharatc@gmail.com>
2021-12-28 08:28:38 +0100

    code

--- serveur.js
+++ serveur.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("./routes/tutorial.routes")(app);
+
 var corsOptions = {
     origin: "http://localhost:8081"
     };
